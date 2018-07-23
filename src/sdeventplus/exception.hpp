eeef46978a092ed688d4930911ae9bebb95e5980
William A. Kennington III <wak@google.com>
2018-07-23 16:33:58 -0700

    exception: Document header

--- src/sdeventplus/exception.hpp
+++ src/sdeventplus/exception.hpp
@@ -5,9 +5,18 @@
 namespace sdeventplus
 {
 
+/** @class SdEventError
+ *  @brief Holds information about underlying sd_event
+ *         issued errors
+ */
 class SdEventError final : public std::system_error
 {
   public:
+    /** @brief Creates a new SdEventError from error data
+     *
+     * @param[in] r      - The postive errno code
+     * @param[in] prefix - The prefix string to display in the what()
+     */
     SdEventError(int r, const char* prefix);
 };
 
